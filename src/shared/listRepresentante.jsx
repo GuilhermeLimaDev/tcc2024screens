3c72af2d50437f7550926728882c758730a739ea
GuilhermeLimaDev <guilhermeguiguigugu2509@gmail.com>
2024-07-18 21:16:51 -0300

    listas

---
+++ src/shared/listRepresentante.jsx
@@ -0,0 +1,28 @@
+const listRepresentante = [
+    {
+        rm: 90001,
+        nome: "Guilherme Santos",
+        turma: "INF2CM",
+        periodo: "manhã",
+        curso: "informática",
+        senha: "202490001"
+    },
+    {
+        rm: 90002,
+        nome: "Manuella Nogueira",
+        turma: "INF2BM",
+        periodo: "manhã",
+        curso: "informática",
+        senha: "202490002"
+    },
+    {
+        rm: 90003,
+        nome: "Pérola Zacarioto",
+        turma: "INF2AM",
+        periodo: "manhã",
+        curso: "informática",
+        senha: "202490003"
+    },
+]
+
+export default listRepresentante
